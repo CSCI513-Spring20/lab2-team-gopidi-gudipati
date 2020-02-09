c8be1fbc9559b57f9a001c8d6af2d7a427cc7813
abhinavreddy07 <reddyabhinav7@gmail.com>
2020-02-09 16:21:35 -0600

    modified

--- Lab2/ColumbusGame/src/Ship.java
+++ Lab2/ColumbusGame/src/Ship.java
@@ -4,59 +4,59 @@ public class Ship {
 int scale = 50;
 int xCell=6;
 int yCell=6;
-	public Point goEast(int x,int y) {
-		if(x<450) {
-			xCell = x+50;
+	public Point goEast(int x,int y) { 	//Method when right arrow is pressed
+		if(x<450) {	// If the ship is in bounds
+			xCell = x+50;	// Move the ship to next right cell
 			 yCell = y;	
 		}
-		else {
-			xCell=xCell;
+		else {		//// If the ship is in bounds do nothing
+			xCell=xCell;	
 			yCell=yCell;
 		}
 		 
-		return new Point(xCell,yCell);
+		return new Point(xCell,yCell);	//Return the positions
 		
 	}
-	public Point goWest(int x,int y) {
-		if(xCell>0) {
-		 xCell = x-50;
+	public Point goWest(int x,int y) {	//Method when left arrow is pressed
+		if(xCell>0) {	// If the ship is in bounds
+		 xCell = x-50;	//Move the ship to the next left cell
 		 yCell = y;
 		}
-		else {
+		else {	// If the ship is not in bounds do nothing
 			xCell=xCell;
 			yCell=yCell;
 		}
-		return new Point(xCell,yCell);
+		return new Point(xCell,yCell);	//Return the posistions
 		
 	}
-	public Point goNorth(int x,int y) {
-		if(yCell>0) {
+	public Point goNorth(int x,int y) {	//Method when up arrow is pressed
+		if(yCell>0) {	// If the ship is in bounds
 		xCell = x;
-		 yCell = y-50;
+		 yCell = y-50;	//Move the ship to the next down cell
 		}
-		else {
+		else {	//// If the ship is not in bounds do nothing
 			xCell=xCell;
 			yCell=yCell;
 		}
-		return new Point(xCell,yCell);
+		return new Point(xCell,yCell);	//Return the positions
 		
 	}
-	public Point goSouth(int x,int y) {
-		if(yCell<450) { 
+	public Point goSouth(int x,int y) {	//Method when down arrow is pressed
+		if(yCell<450) { 	// If the ship is in bounds
 		xCell = x;
-		 yCell = y+50;
+		 yCell = y+50;	//Move the ship to the next upper cell
 		}
-		else {
+		else {	// If the ship is not in bounds do nothing
 			xCell=xCell;
 			yCell=yCell;
 		}
-		return new Point(xCell,yCell);
+		return new Point(xCell,yCell); 	//Return nothing
 		
 	}
 
 	public Point getShipLocation() {
 		
-		return new Point(xCell,yCell);
+		return new Point(xCell,yCell); 		//Return the ships coordinates
 	}
 
 }
